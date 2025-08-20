a5f192e71107cadaa7999702067a34733aaf644d
Thomas Barrial <thomas@barrial.fr>
2025-08-20 17:23:16 -0400

    update

--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -15,7 +15,7 @@ function NavItem({
   return (
     <button
       key={name}
-      className={`${selectedFilter === name.toLowerCase() || pathname === `/${name.toLowerCase()}` ? "bg-black text-white border-y lg:border-x lg:border-white border-black" : "text-black bg-white"} border-y lg:border-x lg:border-y-0 border-black w-full h-full border-dashed cursor-pointer  transform transition-all ease-in duration-300 `}
+      className={`${selectedFilter === name.toLowerCase() || pathname === `/${name.toLowerCase()}` ? "bg-black text-white border-y lg:border-x lg:border-white border-black" : "text-black bg-white"} border-y lg:border-x lg:border-y-0 border-black w-full h-full border-dashed cursor-pointer  transform transition-all ease-in duration-300 px-5`}
       onClick={onClick}
     >
       <ComeUpText height="h-7" text={name} className="lg:flex hidden" />

be20aeb19992ade2c96c9435fe223c7fe455d208
Thomas Barrial <thomas@barrial.fr>
2025-08-28 22:00:23 -0400

    update active Video

--- src/components/video/ActiveVideo.tsx
+++ src/components/video/ActiveVideo.tsx
@@ -31,15 +31,26 @@ function ActiveVideo({ activeVideo, videoRef, setActiveVideo }: IProps) {
       className={`fixed z-50 top-0 left-0 flex flex-col items-center justify-center h-full w-full bg-black lg:p-10 lg:pt-5 p-2 ${isAnimated ? "opacity-100" : "opacity-0"} transition-opacity duration-[2000ms] h-screen`}
     >
       <div
-        className={`flex w-full justify-between uppercase px-5 py-4 ${isAnimated ? "translate-y-0" : "-translate-y-100"} transition-all ease-in-out duration-[2000ms]`}
+        className={`${isAnimated ? "translate-y-0" : "-translate-y-100"} transition-all ease-in-out duration-[2000ms]  flex flex-row w-full items-end justify-between`}
       >
-        <h3>{activeVideo.title}</h3>
-        <button
-          onClick={onClose}
-          className="text-white text-sm z-50 cursor-pointer border-b hover:font-bold  transition-all duration-500"
+        <div
+          className={`flex  flex-row w-full space-x-1 uppercase px-5 lg:px-0 items-center`}
         >
-          CLOSE X
-        </button>
+          <h3>{activeVideo.title} /</h3>
+          {activeVideo.categories.map((cat) => (
+            <h3 className="text-sm opacity-50" key={cat}>
+              {cat}
+            </h3>
+          ))}
+        </div>
+        <div className={`w-full flex justify-end px-5 pt-4 `}>
+          <button
+            onClick={onClose}
+            className="text-white text-sm z-50 cursor-pointer border-b hover:font-bold  transition-all duration-500"
+          >
+            CLOSE X
+          </button>
+        </div>
       </div>
       <video
         src={`${activeVideo.video}#t=0.001`}
